086fcc713932620eed1c5558d90dce7975d2d260
Vuong Ho Ngoc <34616024+hongocvuong1998@users.noreply.github.com>
2019-05-23 00:17:59 -0700

    Add files via upload

--- Rename.py
+++ Rename.py
@@ -1,5 +1,5 @@
 import os
-
+import cv2
 def Rename(src,first_name,file_format):
     list_file=os.listdir(src)
 
@@ -9,5 +9,25 @@ def Rename(src,first_name,file_format):
         os.rename(file_src,file_dst)
         print(file_src)
 
-Rename("/home/vuonghn/Downloads/Data_Pneumonia/chest_xray/val/PNEUMONIA","PNEUMONIA_",".jpg")
+def RemoveFile(src):
+    list_file=os.listdir(src)
+    for i in range(len(list_file)):
+        file_src=src+'/'+list_file[i]
+        img=cv2.imread(file_src)
+        print(type(img))
+        if str(type(img)) != "<class 'numpy.ndarray'>":
+            os.remove(file_src)
+            continue
+
+
+root="/home/vuonghn/Downloads/ALL/all"
+
+list_folder=os.listdir(root)
+
+for i in range(len(list_folder)):
+        folder=root+'/'+list_folder[i]
+        # Rename(folder,"img_",".jpg")
+        RemoveFile(folder)    
+
+
 
