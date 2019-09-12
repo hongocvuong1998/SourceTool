6994f6b529eb62b5e2c37ddc9cde692fdda595c5
vuonghn <vuonghn@admicro.vn>
2019-09-12 17:33:01 +0700

    I had write something in file

--- feature.py
+++ feature.py
@@ -1,7 +1,2 @@
 def featureA():
-    print("I want get a lot of money, oh oh, can you help me")
-    print("Do you have money, Can I borow it.....")
-    print("yes, 500$")
-    print("I dont't")
-    print("anh an")
-    print("anh em")
+    print("I using function feature A")
