1dbc3593a887a9949ae8b9622569af80c11f78fa
vuonghn <vuonghn@admicro.vn>
2019-09-12 17:24:08 +0700

    goo

---
+++ feature.py
@@ -0,0 +1,6 @@
+def featureA():
+    print("I want get a lot of money, oh oh, can you help me")
+    print("Do you have money, Can I borow it.....")
+    print("yes, 500$")
+    print("I dont't")
+    print("anh an")
